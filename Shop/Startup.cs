a0f7361d0602d4dad40ba9da93eff1e95501bff6
Ivan Gulin <doctor254452009@ya.ru>
2016-01-15 22:36:13 +0500

    Добавлена аутентификация пользователей!

---
+++ Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autentification.Startup))]
+namespace Autentification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
