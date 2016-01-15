456b660586e376ae2491db2e3c7058af91afbb0e
doctor2 <doctor254452009@ya.ru>
2016-01-15 14:23:33 +0500

    Реализована сортировка из базы(Ajax)
    При помощи выпадающего списка

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -22,12 +22,30 @@ namespace Shop.Controllers
 
         public ActionResult Computers()
         {
-            return View(db.Comp.ToList());
+            return View(); //db.Comp.ToList()
         
         }
         public ActionResult Computers2()
         {
             return View(db.Comp.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Computers(string id)
+        {
+            // id - имя клиента, заказы которого необходимо выводить на странице.
+            return View("Computers", (object)id);
+        }
+
+        public ActionResult OrdersData(string id)
+        {
+            var data = db.Comp.ToList();
+            if (!string.IsNullOrEmpty(id) && id != "All")
+            {
+                // выполняем выборку по свойству Customer если значение id не пустое и не равное "All"
+                data = data.Where(e => e.Firm == id).ToList();
+            }
+            return PartialView(data);
         }
 	}
 }
