fa31c5cda23c64af0210e04b81ebb472cd3dddc8
doctor2 <doctor254452009@ya.ru>
2016-01-19 11:52:37 +0500

    Добавленны bundle для оптимизации
    Не все скрипты в бандлах

--- Shop/Global.asax.cs
+++ Shop/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Shop
@@ -16,6 +17,7 @@ namespace Shop
             Database.SetInitializer(new ItemDbInitializer());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
